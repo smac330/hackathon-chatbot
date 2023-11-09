a796bb8c72d401d8e9486b1451f42c238e5f9db1
smac330 <117689648+smac330@users.noreply.github.com>
2023-11-09 15:27:29 -0500

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,1 +1,19 @@
-print("hello world")
+import streamlit as st
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to Streamlit! 👋")
+
+    st.markdown(
+      """
+       Welcome to the page!
+      """
+    )
+
+
+if __name__ == "__main__":
+    run()
