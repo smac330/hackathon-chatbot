e7db032a9992e3ac6b3ff13bbbc479e08ac88b4a
smac330 <117689648+smac330@users.noreply.github.com>
2023-11-09 16:11:14 -0500

    Update1 prog1.py

--- prog1.py
+++ prog1.py
@@ -2,17 +2,19 @@ import streamlit as st
 
 def run():
     st.set_page_config(
-        pagetitle="VSU ChatBot",
-        pageIcon="👋",
+        page_title="Hello",
+        page_icon="👋",
         )
 
-    st.write("# Welcome to the VSU ChatBot, Yappers!👋")
+    st.write("# Welcome to Streamlit! 👋")
 
     st.markdown(
       """
-         Yappers is here to help you with your classes and give encouragement to every student. Please enter a question in the chat box below!
+       Welcome to the VSU ChatBot, Yappers! Yappers is here to help you with your classes and give encouragement to every student. Please enter a question in the chat box below!
       """
     )
 
+
 if __name__ == "__main__":
     run()
+    
