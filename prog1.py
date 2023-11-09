fc46fbbb6173c577c9ce74ca3c0712ef9596e66f
smac330 <117689648+smac330@users.noreply.github.com>
2023-11-09 16:49:08 -0500

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 def run():
     st.set_page_config(
         page_title="VSU ChatBot",
