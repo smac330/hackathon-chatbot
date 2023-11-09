1b544d0715d0967f88018682741722be69b5b74a
smac330 <117689648+smac330@users.noreply.github.com>
2023-11-09 15:43:36 -0500

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def run():
-    st.set_page_config(
-        page_title="VSU ChatBot",
-        page_icon="👋",
+    st.setPageConfig(
+        pagetitle="VSU ChatBot",
+        pageIcon="👋",
         )
 
     st.write("# Welcome to the VSU ChatBot, Yappers!👋")
@@ -13,6 +13,9 @@ def run():
          Yappers is here to help you with your classes and give encouragement to every student. Please enter a question in the chat box below!
       """
     )
+
+    question = st.textInput("Enter Question: ", "What's your major?")
+    st.write( "The question you asked was, " question)
 
 
 if __name__ == "__main__":
